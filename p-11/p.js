bcfd0c40797a4560eace3632b2cd33012bfc5a70
Admin <hemamane5376@gmail.com>
2022-08-24 00:43:22 +0530

    P-12

---
+++ p-11/p.js
@@ -0,0 +1,12 @@
+////////////////////////// selector ///////////////////////////////
+let btn1 =document.querySelector(".pre")
+let btn2=document.querySelector(".next")
+
+/////////////////////////// container ///////////////////////////
+
+btn1.addEventListener("click",previous)
+console.log(btn1);
+
+function previous(){
+    console.log(previous);
+}
